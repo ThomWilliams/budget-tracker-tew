10558459739aef9f7a668e561b7422309a71e35d
Thom Williams <thomwilliams1990@gmail.com>
2021-07-08 23:35:47 +0100

    DB.js updated

--- public/db.js
+++ public/db.js
@@ -12,6 +12,8 @@ const request = indexedDB.open("budget", 1);
 // UPGRADE ON REQUEST
 request.onupgradeneeded = ({ target }) => {
   console.log("Upgrade IndexDB");
+  db = target.result;
+  db.createObjectStore("BudgetStore", { autoIncrement: true });
 
   // const { olderVersion } = e;
   // const newerVersion = e.newerVersion || db.version;
@@ -27,74 +29,73 @@ request.onupgradeneeded = ({ target }) => {
 
 // SUCCESS REQUEST
 request.onsuccess = ({ target }) => {
-    db = request.result;
-    tx = db.transaction("BudgetStore", "readWrite")
-    store = tx.objectStore("BudgetStore")
-    console.log(target, "Request Successful!")
-    saveRecord();
+  db = target.result;
+
+  if (navigator.onLine) {
+    console.log("Backend online!");
+    checkLiveDatabase();
+  }
+
 };
 
 // request.onsuccess = function (e) {
 //     console.log('success');
 //     db = e.target.result;
-  
+
 //     // Check if app is online before reading from db
-//     if (navigator.onLine) {
-//       console.log('Backend online!');
-//       checkDatabase();
-//     }
+//
 //   };
 
 // ERROR REQUEST
 request.onerror = function (event) {
-    console.log(`Error processing request! ${event.target.errorCode}`);
+  console.log(`Error processing request! ${event.target.errorCode}`);
 };
 
 // SAVE RECORD
 function saveRecord(record) {
-    console.log('Save record invoked');
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
-  const store = transaction.objectStore('BudgetStore');
-  // Add record to store 
+  console.log("Save record invoked");
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+  // Add record to store
   store.add(record);
 }
 
 // CHECK DATABASE
-function checkDatabase() {
-    console.log('check db invoked');
-    let transaction = db.transaction(['BudgetStore'], 'readwrite');
-    const store = transaction.objectStore('BudgetStore');
-  
-    // Get all records from store 
-    const getAll = store.getAll();
-  
-    // If the request was successful
-    getAll.onsuccess = function () {
-      if (getAll.result.length > 0) {
-        fetch('/api/transaction/bulk', {
-          method: 'POST',
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => response.json())
-          .then((res) => {
-            // If returned response is not empty, Open another transaction
-            if (res.length !== 0) {
-              transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
-              // Assigns current store to a currentStore variable
-              const currentStore = transaction.objectStore('BudgetStore');
-  
-              // Clears existing entries 
-              currentStore.clear();
-              console.log('Store cleared');
-            }
-          });
-      }
-    };
-  }
+function checkLiveDatabase() {
+  console.log("check db invoked");
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
 
-window.addEventListener("online", checkDatabase);
+  // Get all records from store
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If returned response is not empty, Open another transaction
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            // Assigns current store to a currentStore variable
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            // Clears existing entries
+            currentStore.clear();
+            console.log("Store cleared");
+          }
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkLiveDatabase);
